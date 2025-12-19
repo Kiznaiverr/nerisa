50c45fa1d4ffc128faa861b641c7a03a0a665acf
Kiznavierr <atqiyaakmal24@gmail.com>
2025-12-19 07:38:50 +0700

    feat: add penghitaman

---
+++ plugins/ai/hitamkan.js
@@ -0,0 +1,52 @@
+module.exports = {
+   help: ['hitamkan'],
+   use: 'reply media',
+   tags: 'ai',
+   run: async (m, {
+      conn,
+      text,
+      usedPrefix,
+      command,
+      Func,
+      Scraper
+   }) => {
+      try {
+         const prompt = text ? text : 'ubah warna kulitnya jadi hitam'
+         let old = new Date()
+         if (m.quoted ? m.quoted.message : m.msg.viewOnce) {
+            let type = m.quoted ? Object.keys(m.quoted.message)[0] : m.mtype
+            let q = m.quoted ? m.quoted.message[type] : m.msg
+            let img = await conn.downloadMediaMessage(q)
+            if (!/image/.test(type)) throw Func.texted('bold', `Stress ??`)
+            conn.sendReact(m.chat, '🕒', m.key)
+            const srv = await Scraper.uploader(img)
+            if (!srv.status) throw Func.jsonFormat(srv)
+            const json = await Api.get('/ai-edit', {
+               image: srv.data.url,
+               prompt: prompt
+            })
+            if (!json.status) throw Func.jsonFormat(json)
+            conn.sendFile(m.chat, json.data.images[0].url, '', `🍟 *Process* : ${((new Date - old) * 1)} ms`, m)
+         } else {
+            let q = m.quoted ? m.quoted : m
+            let mime = (q.msg || q).mimetype || ''
+            if (!/image\/(jpe?g|png)/.test(mime)) throw Func.texted('bold', `Stress ??`)
+            let img = await q.download()
+            if (!img) throw global.status.wrong
+            conn.sendReact(m.chat, '🕒', m.key)
+            const srv = await Scraper.uploader(img)
+            if (!srv.status) throw Func.jsonFormat(srv)
+            const json = await Api.get('/ai-edit', {
+               image: srv.data.url,
+               prompt: prompt
+            })
+            if (!json.status) throw Func.jsonFormat(json)
+            conn.sendFile(m.chat, json.data.images[0].url, '', `🍟 *Process* : ${((new Date - old) * 1)} ms`, m)
+         }
+      } catch (e) {
+         throw Func.jsonFormat(e)
+      }
+   },
+   error: false,
+   limit: 5
+}
