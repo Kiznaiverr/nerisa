48223c42dc728afd83033984df469b47ab3cd7dc
Kiznavierr <atqiyaakmal24@gmail.com>
2025-12-22 12:48:22 +0700

    feat: add discord CDN

--- plugins/tools/upload.js
+++ plugins/tools/upload.js
@@ -14,10 +14,16 @@ module.exports = {
          if (!mime) throw Func.texted('bold', '🚩 Send or reply to the media you want to upload.')
          conn.sendReact(m.chat, '🕒', m.key)
          let media = await q.download()
-         //let isMedia = /image\/(png|jpe?g|gif)|video\/mp4\/webp/.test(mime)
-         //let json = await (isMedia ? Scraper.imgbb : Scraper.uploader)(media)
-         let json = await Scraper.tmpfiles(media)
-         conn.reply(m.chat, json.data.url, m)
+         let fileName = `upload_${Date.now()}`
+         // Try Discord CDN first
+         let json = await require('../../lib/system/discordCDN')(media, fileName)
+         if (json.status && json.data.result.attachments && json.data.result.attachments[0]) {
+            conn.reply(m.chat, json.data.result.attachments[0].url, m)
+         } else {
+            // Fallback to tmpfiles
+            json = await Scraper.tmpfiles(media)
+            conn.reply(m.chat, json.data.url, m)
+         }
       } catch (e) {
          throw Func.jsonFormat(e)
       }
