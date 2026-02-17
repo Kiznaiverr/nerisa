274f91569d075df2ed5000edff9f9785c48fadb2
Kiznavierr <atqiyaakmal24@gmail.com>
2026-02-17 18:40:42 +0700

    feat: implement Sholat Scheduler

--- main.js
+++ main.js
@@ -1,84 +1,132 @@
-const { Connection, Database, Function: Func, Config: env } = require('@znan/wabot')
-require('./lib/system/config'), require('./lib/system/function'), require('./lib/system/scraper')
-const fs = require('fs')
-const config = require('./config.json')
+const {
+  Connection,
+  Database,
+  Function: Func,
+  Config: env,
+} = require("@znan/wabot");
+(require("./lib/system/config"),
+  require("./lib/system/function"),
+  require("./lib/system/scraper"));
+const fs = require("fs");
+const config = require("./config.json");
 
 const connect = async () => {
-   const url = process?.env?.DATABASE_URL
-   const system = Database.create(url, env.database)
+  const url = process?.env?.DATABASE_URL;
+  const system = Database.create(url, env.database);
 
-   const conn = new Connection({
-      plugins_dir: 'plugins',
-      session_dir: system.session ? system.session : 'session',
+  const conn = new Connection(
+    {
+      plugins_dir: "plugins",
+      session_dir: system.session ? system.session : "session",
       online: true,
       presence: true,
       bypass_ephemeral: true,
       pairing: config.pairing,
-   }, {
-      browser: ['Ubuntu', 'Firefox', '20.0.00'],
+    },
+    {
+      browser: ["Ubuntu", "Firefox", "20.0.00"],
       version: [2, 3000, 1032750324],
-      shouldIgnoreJid: jid => {
-         return /(newsletter|bot)/.test(jid)
+      shouldIgnoreJid: (jid) => {
+        return /(newsletter|bot)/.test(jid);
+      },
+    },
+  );
+
+  conn.once("connect", async (x) => {
+    /** load db */
+    global.db = {
+      users: {},
+      groups: {},
+      chats: {},
+      setting: {},
+      statistic: {},
+      sticker: {},
+      ...((await system.database.fetch()) || {}),
+    };
+    /** save db */
+    await system.database.save(global.db);
+    /** write log */
+    if (x && typeof x === "object" && x.message)
+      console.log(x.display, x.message);
+  });
+
+  conn.on("error", (err) => {
+    if (err.display) console.log(err.display, err.message);
+    else console.log(err.message);
+    if (err.message) Func.logFile(err.message);
+  });
+
+  conn.once("prepare", async (x) => {
+    /** write log */
+    console.log(x.display, x.message);
+
+    /** initialize sholat scheduler */
+    try {
+      global.sholatScheduler = require("./lib/sholat-scheduler")(
+        conn,
+        env,
+        Func,
+      );
+    } catch (e) {
+      console.error("Failed to initialize Sholat Scheduler:", e.message);
+    }
+
+    /* auto restart if ram usage is over */
+    const ramCheck = setInterval(() => {
+      var ramUsage = process.memoryUsage().rss;
+      if (ramUsage >= require("bytes")(env.ram_limit)) {
+        clearInterval(ramCheck);
+        process.send("reset");
       }
-   })
+    }, 60 * 1000);
 
-   conn.once('connect', async x => {
-      /** load db */
-      global.db = { users: {}, groups: {}, chats: {}, setting: {}, statistic: {}, sticker: {}, ...(await system.database.fetch() || {}) }
-      /** save db */
-      await system.database.save(global.db)
-      /** write log */
-      if (x && typeof x === 'object' && x.message) console.log(x.display, x.message)
-   })
+    /* create temp directory if doesn't exists */
+    if (!fs.existsSync("./tmp")) fs.mkdirSync("./tmp");
 
-   conn.on('error', err => {
-      if (err.display) console.log(err.display, err.message)
-      else console.log(err.message)
-      if (err.message) Func.logFile(err.message)
-   })
+    /* clear temp folder every 12 hour */
+    setInterval(
+      async () => {
+        try {
+          const tmpFiles = fs.readdirSync("./tmp");
+          if (tmpFiles.length > 0) {
+            tmpFiles
+              .filter((v) => !v.endsWith(".file"))
+              .map((v) => fs.unlinkSync("./tmp/" + v));
+          }
+        } catch {}
+      },
+      12 * 60 * 60 * 1000,
+    );
 
-   conn.once('prepare', async x => {
-      /** write log */
-      console.log(x.display, x.message)
+    /** save database every 2 min */
+    setInterval(async () => {
+      if (global.db) await system.database.save(global.db);
+    }, 30_000);
 
-      /* auto restart if ram usage is over */
-      const ramCheck = setInterval(() => {
-         var ramUsage = process.memoryUsage().rss
-         if (ramUsage >= require('bytes')(env.ram_limit)) {
-            clearInterval(ramCheck)
-            process.send('reset')
-         }
-      }, 60 * 1000)
+    /* backup database every 2 hour (send .json file to owner) */
+    setInterval(
+      async () => {
+        if (global?.db?.setting?.autobackup) {
+          await system.database.save(global.db);
+          fs.writeFileSync(
+            env.database + ".json",
+            JSON.stringify(global.db, null, null),
+            "utf-8",
+          );
+          await conn.sock.sendFile(
+            env.owner + "@s.whatsapp.net",
+            fs.readFileSync("./" + env.database + ".json"),
+            env.database + ".json",
+            "",
+            null,
+          );
+        }
+      },
+      2 * 60 * 60 * 1000,
+    );
 
-      /* create temp directory if doesn't exists */
-      if (!fs.existsSync('./tmp')) fs.mkdirSync('./tmp')
-
-      /* clear temp folder every 12 hour */
-      setInterval(async () => {
-         try {
-            const tmpFiles = fs.readdirSync('./tmp')
-            if (tmpFiles.length > 0) {
-               tmpFiles.filter(v => !v.endsWith('.file')).map(v => fs.unlinkSync('./tmp/' + v))
-            }
-         } catch { }
-      }, 12 * 60 * 60 * 1000)
-
-      /** save database every 2 min */
-      setInterval(async () => {
-         if (global.db) await system.database.save(global.db)
-      }, 30_000)
-
-      /* backup database every 2 hour (send .json file to owner) */
-      setInterval(async () => {
-         if (global?.db?.setting?.autobackup) {
-            await system.database.save(global.db)
-            fs.writeFileSync(env.database + '.json', JSON.stringify(global.db, null, null), 'utf-8')
-            await conn.sock.sendFile(env.owner + '@s.whatsapp.net', fs.readFileSync('./' + env.database + '.json'), env.database + '.json', '', null)
-         }
-      }, 2 * 60 * 60 * 1000)
-
-      /** listeners */
-      require('./lib/system/listeners')(conn, system)
-   })
-}
-connect().catch(() => connect())
+    /** listeners */
+    require("./lib/system/listeners")(conn, system);
+  });
+};
+connect().catch(() => connect());
